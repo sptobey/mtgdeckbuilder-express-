42958e8d6c407b520ec45930880243adc48905ad
Haoran Fang <gougou9903@gmail.com>
2015-04-11 02:05:00 -0600

    get data
    using http library can get the data!

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var $ = require('jquery');
+var http = require('http');
 
 /* 
 var dburl = ''
@@ -14,7 +16,29 @@ app.get('/', function(req, res){
 
 //app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
-require('./routes/cardFilter')(app)
+//require('./routes/cardFilter')(app)
+
+app.get('/cards/cardSearch', function(req, res){
+        
+        // https://api.mtgdb.info/content/hi_res_card_images/{multiverseid}.jpg
+        // env(html, function (errors, window){
+        // 	var $ = require('jquery')(window)
+	       //  $.get("data/data.json", function(data) {
+	       //      console.log("Random Card:", data);
+	       //  });
+
+        // })
+
+		http.get("http://api.mtgdb.info/cards/random", function(res){
+			res.on('data', function(data){
+				console.log('BODY: ' + data)
+			})
+		})
+
+
+        res.render('cards/cardSearch.jade', {
+        })
+    })
 
 
 app.set('port', (process.env.PORT || 3000))
