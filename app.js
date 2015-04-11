eca8f848186e0f5c2d01ca1120f55317ccc109b4
Samuel Tobey <Samuel.P.Tobey@colorado.edu>
2015-04-11 11:30:49 -0600

    move random card get to cardFileter

--- app.js
+++ app.js
@@ -14,32 +14,7 @@ app.get('/', function(req, res){
     res.render('index.jade')
 })
 
-//app.use('jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-
-//require('./routes/cardFilter')(app)
-
-app.get('/cards/cardSearch', function(req, res){
-        
-        // https://api.mtgdb.info/content/hi_res_card_images/{multiverseid}.jpg
-        // env(html, function (errors, window){
-        // 	var $ = require('jquery')(window)
-	       //  $.get("data/data.json", function(data) {
-	       //      console.log("Random Card:", data);
-	       //  });
-
-        // })
-
-		http.get("http://api.mtgdb.info/cards/random", function(res){
-			res.on('data', function(data){
-				console.log('BODY: ' + data)
-			})
-		})
-
-
-        res.render('cards/cardSearch.jade', {
-        })
-    })
-
+require('./routes/cardFilter')(app)
 
 app.set('port', (process.env.PORT || 3000))
 
--- routes/cardFilter.js
+++ routes/cardFilter.js
@@ -1,13 +1,23 @@
 
 module.exports = function(app){
     
+    var http = require('http');
+    
     app.get('/cards/cardSearch', function(req, res){
         
         // https://api.mtgdb.info/content/hi_res_card_images/{multiverseid}.jpg
-        
-        // $.get("http://api.mtgdb.info/cards/random", function(data) {
-        //     console.log("Random Card:", data);
-        // });
+        // env(html, function (errors, window){
+        //     var $ = require('jquery')(window)
+        //     $.get("data/data.json", function(data) {
+        //         console.log("Random Card:", data);
+        //     });
+        // })
+
+        http.get("http://api.mtgdb.info/cards/random", function(res){
+            res.on('data', function(data){
+                console.log('BODY: ' + data)
+            })
+        })
         
         res.render('cards/cardSearch.jade', {
         })
